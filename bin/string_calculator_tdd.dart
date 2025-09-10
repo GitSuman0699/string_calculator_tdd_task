d1d994470c5526f59a5cfa0500b9e0224f8f229a
SumanUtahTech <suman.k@utahtechlab.com>
2025-09-10 15:23:27 +0530

    first commit

---
+++ bin/string_calculator_tdd.dart
@@ -0,0 +1,6 @@
+import 'package:string_calculator_tdd/string_calculator_tdd.dart'
+    as string_calculator_tdd;
+
+void main() {
+  var calculator = string_calculator_tdd.StringCalculator();
+}
---
+++ test/string_calculator_tdd_test.dart
@@ -0,0 +1,12 @@
+import 'package:string_calculator_tdd/string_calculator_tdd.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('StringCalculator', () {
+    late StringCalculator calculator;
+
+    setUp(() {
+      calculator = StringCalculator();
+    });
+  });
+}
