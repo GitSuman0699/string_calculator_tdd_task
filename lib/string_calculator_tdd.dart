aa4972a5b177ea501b7af631d2a9354745e93b49
SumanUtahTech <suman.k@utahtechlab.com>
2025-09-10 15:43:48 +0530

    Refactor: handled sum for two comma-separated numbers.

--- lib/string_calculator_tdd.dart
+++ lib/string_calculator_tdd.dart
@@ -1,7 +1,7 @@
 class StringCalculator {
   int add(String numbers) {
     if (numbers.isEmpty) return 0;
-    if (numbers.length == 1) return int.parse(numbers);
-    return 0;
+    if (!numbers.contains(',')) return int.parse(numbers);
+    return numbers.split(',').map(int.parse).reduce((a, b) => a + b);
   }
 }
