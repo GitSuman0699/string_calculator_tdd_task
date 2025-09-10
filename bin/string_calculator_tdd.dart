19163eaeb1d1bb47df0794e085259e53f183ecde
SumanUtahTech <suman.k@utahtechlab.com>
2025-09-10 15:55:01 +0530

    Green: support both comma and newline as delimiters in add method.

--- bin/string_calculator_tdd.dart
+++ bin/string_calculator_tdd.dart
@@ -7,4 +7,5 @@ void main() {
   print('Single number: ${calculator.add('1')}'); // 1
   print('Two numbers: ${calculator.add('1,2')}'); // 3
   print('Multiple numbers: ${calculator.add('1,2,3,4,5')}'); // 15
+  print('Numbers with newlines: ${calculator.add('1\n2,3')}'); // 6
 }
--- lib/string_calculator_tdd.dart
+++ lib/string_calculator_tdd.dart
@@ -1,7 +1,12 @@
 class StringCalculator {
   int add(String numbers) {
     if (numbers.isEmpty) return 0;
-    if (!numbers.contains(',')) return int.parse(numbers);
-    return numbers.split(',').map(int.parse).reduce((a, b) => a + b);
+    numbers = numbers.replaceAll('\n', ',');
+
+    return numbers
+        .split(',')
+        .where((s) => s.isNotEmpty)
+        .map(int.parse)
+        .reduce((a, b) => a + b);
   }
 }
